b495a7c110b24bce5ff8611c878fd1deeccb5bb8
Salim Aydın <salimaydin340@gmail.com>
2023-03-03 23:13:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(10);
+  const [rastgele, setRastgele] = useState(0);
+  
+  const handleSubmit = () => {
+    if (min < max) {
+      setRastgele(Math.floor(Math.random() * (max - min + 1)) + min);
+    }else{
+      alert("A sayısı B sayısından küçük olmalıdır.")
+    }
+  }
+
+  const handleChangeMin = (e) => {
+    setMin(parseInt(e.target.value,10))
+  }
+
+  const handleChangeMax = (e) => {
+    setMax(parseInt(e.target.value,10))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="ilk">Random Number {rastgele}</div>
+      <div id="orta">
+        <div>MIN</div>
+        <input type="number" onChange={handleChangeMin} value={min}/>
+        <div>MAX</div>
+        <input type="number" onChange={handleChangeMax} value={max}/>
+      </div>
+      <div id="son">
+        <button onClick={handleSubmit}>Generate</button>
+      </div>
     </div>
   );
 }
